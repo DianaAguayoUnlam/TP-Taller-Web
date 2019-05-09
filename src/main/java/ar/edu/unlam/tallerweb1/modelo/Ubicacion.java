f29123647ecc6978c1896d008f3c421cc15ffba0
DianaAguayo <ddianaosi15@gmail.com>
2019-05-09 00:35:40 -0300

    clase ubicacion

---
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Ubicacion.java
@@ -0,0 +1,68 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+
+@Entity
+public class Ubicacion {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Integer latitud;
+	private Integer longitud;
+	
+	@OneToOne
+	private Ciudad ciudad;
+	
+//	@OneToOne
+//	private Continente continente;
+//	
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	
+
+	public Integer getLatitud() {
+		return latitud;
+	}
+
+	public void setLatitud(Integer latitud) {
+		this.latitud = latitud;
+	}
+
+	public Integer getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(Integer longitud) {
+		this.longitud = longitud;
+	}
+
+	public Ciudad getCiudad() {
+		return ciudad;
+	}
+
+
+
+	public void setCiudad(Ciudad ciudad) {
+		this.ciudad = ciudad;
+	}
+
+
+
+}
